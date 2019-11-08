0f0d169c49d16a7e9e7bb116b0a67120c8fa477b
中島 祐也 <zeema_1995@yahoo.co.jp>
2019-11-08 18:07:51 +0900

    新規

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!!!");
+    System.out.println("Hello World!!!");
+
+    if (0 == 0) {
+      System.out.println("asa");
+      System.out.println("asa2");
+    }
+  }
+}
